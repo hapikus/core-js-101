56bdadf7e7a835e7f85a2e3c0ea672496d179cf9
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-06-02 19:39:57 +0700

    feat: solve parth of 08

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,10 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return function newOne(x) {
+    return f(g(x));
+  };
 }
 
 
@@ -44,8 +46,10 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function powerExp(num) {
+    return num ** exponent;
+  };
 }
 
 
@@ -62,8 +66,19 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return function calculate(x) {
+    if (args.length === 0) {
+      return null;
+    }
+    if (args.length === 1) {
+      return args.at(-1);
+    }
+    if (args.length === 2) {
+      return args.at(-2) * x + args.at(-1);
+    }
+    return args.at(-3) * (x ** 2) + args.at(-2) * x + args.at(-1);
+  };
 }
 
 
@@ -169,8 +184,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = 0;
+  return function nextGeneratior() {
+    count += 1;
+    return startFrom + count - 1;
+  };
 }
 
 
